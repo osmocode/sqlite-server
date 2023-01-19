fe0d7a14d223a56547404b1eb4acf8f822870456
osmocode <96887741+osmocode@users.noreply.github.com>
2023-01-19 23:40:01 +0100

    update: package

--- setup.py
+++ setup.py
@@ -3,8 +3,12 @@ from setuptools import find_packages, setup
 setup(
     name='sqlite_server',
     packages=find_packages(include=['sqlite_server']),
-    version='0.1.0',
+    version='0.1.2',
     description='On request SQLite server',
     author='osmocode',
-    license='MIT'
+    license='MIT',
+    install_requires=['Pyro5'],
+    setup_requires=[''],
+    tests_require=[''],
+    test_suite='tests',
 )
