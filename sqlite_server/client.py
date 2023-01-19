6147b0f282ba316bc8f475977618f676f7729613
osmocode <96887741+osmocode@users.noreply.github.com>
2023-01-19 23:39:27 +0100

    update: set remote on init

--- sqlite_server/client.py
+++ sqlite_server/client.py
@@ -1,23 +1,17 @@
-from dataclasses import dataclass, field
 from typing import Any
 from .exceptions import *
-import Pyro5.client
+import Pyro5.api
 
 
-@dataclass
-class SQLiteClient:
+class SQLiteClient():
 
-    name: str
-    host: str = field(default='localhost')
-    port: int = field(default=4242)
-    remote: Pyro5.client.Proxy = field(default=None)
-
-    def connect(self):
-        if self.remote != None:
-            raise ProxyIllegalStateException('Connection already exist...')
-        self.remote = Pyro5.client.Proxy(f'PYRO:{self.name}@{self.host}:{self.port}')
-    
+    def __init__(self,name: str, **kwargs) -> None:
+        self.name = name
+        self.host = kwargs.get('host', 'localhost')
+        self.port = kwargs.get('port', 4242)
+        self.remote = Pyro5.api.Proxy(f'PYRO:{self.name}@{self.host}:{self.port}')
+        
     def request(self, uri: str, method: str, **kwargs) -> Any:
         if self.remote == None:
             raise ProxyIllegalStateException('Any connection with proxy...')
-        self.remote.request(uri, method, kwargs=kwargs)
+        return self.remote.request(uri, method, **kwargs)
