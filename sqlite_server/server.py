6455f77458d4babf3bce02fa53148803e76d132d
osmocode <96887741+osmocode@users.noreply.github.com>
2023-01-19 23:38:27 +0100

    feat: dynamically load module and call method

--- sqlite_server/server.py
+++ sqlite_server/server.py
@@ -1,21 +1,36 @@
-from dataclasses import dataclass, field
-from typing import Any
-import Pyro5.server
+import Pyro5.api
+import importlib
 
-@Pyro5.server.expose
-@dataclass
-class SQLiteServer:
+
+@Pyro5.api.expose
+class SQLiteServer():
     
-    host: str = field(default='localhost')
-    port: int = field(default=4242)
+    def __init__(self, directory: str, **kwargs) -> None:
+        self.directory = directory
+        self.name = kwargs.get('name', 'sqlite-server')
+        self.host = kwargs.get('host', 'localhost')
+        self.port = kwargs.get('port', 4242)
+        with Pyro5.api.Daemon(self.host, self.port) as deamon:
+            uri = deamon.register(self, self.name)
+            print(f"""
+SQLite Server verison 0.1.2
+Starting server at: {uri}
+Quit the server with CONTROL-C.
+            """)
+            deamon.requestLoop()
 
     def _start(self):
         print('Starting server...')
-        with Pyro5.server.Daemon(self.host, self.port) as deamon:
-            uri = deamon.register(self, 'sqlite-server')
+        with Pyro5.api.Daemon(self.host, self.port) as deamon:
+            uri = deamon.register(self, self.name)
             print(uri)
             deamon.requestLoop()
 
-    def request(self, uri: str, method: str, **kwargs: dict[str, Any]):
-        print(f'Request: {uri}, method: {method}')
-        print(kwargs)
+    def request(self, uri: str, method: str, **kwargs):
+        module_dir = self.directory.replace("/", ".")
+        module_name = uri.replace("/", ".")
+        try:
+            module = importlib.import_module(f"{module_dir}.{module_name}")
+            return getattr(module, method)(kwargs)
+        except Exception:
+            return f"Could not import module '{module_name}'"
